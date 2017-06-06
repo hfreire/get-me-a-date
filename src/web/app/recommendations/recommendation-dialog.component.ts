435bd108b989693c7bb0565d8a817b479f3503b1
Hugo Freire <hfreire@exec.sh>
2017-06-06 22:52:50 +0200

    style: minor tweaks

--- src/web/app/recommendations/recommendation-dialog.component.ts
+++ src/web/app/recommendations/recommendation-dialog.component.ts
@@ -9,15 +9,28 @@ import { RecommendationsService } from './recommendations.service'
 
 import { Component, Inject } from '@angular/core'
 import { MD_DIALOG_DATA, MdDialogRef } from '@angular/material'
+import { animate, state, style, transition, trigger } from '@angular/animations'
 
 import * as _ from 'lodash'
 
 @Component({
   selector: 'recommendation-dialog',
   templateUrl: '/app/recommendations/recommendation-dialog.html',
-  providers: [ RecommendationsService ]
+  providers: [ RecommendationsService ],
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({ opacity: 1, visibility: 'visible' })),
+      state('out', style({ opacity: 0, visibility: 'hidden' })),
+      transition('in <=> out', [
+        animate('1s ease-out')
+      ])
+    ])
+  ]
 })
 export class RecommendationDialogComponent {
+  fadeInState = 'in'
+  fadeOutState = 'out'
+
   occupation: any
   private today: Date
   private recommendation: any
@@ -28,6 +41,11 @@ export class RecommendationDialogComponent {
     this.today = new Date()
     this.recommendation = this.data.recommendation
     this.occupation = _.has(this.recommendation, 'data.schools[0].name') ? this.recommendation.data.schools[ 0 ].name : _.has(this.recommendation, 'data.jobs[0].company.name') ? this.recommendation.data.jobs[ 0 ].company.name : ''
+  }
+
+  public isLoaded (event: Event) {
+    this.fadeInState = 'out'
+    this.fadeOutState = 'in'
   }
 
   trainRecommendation () {
