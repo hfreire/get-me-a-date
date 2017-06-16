184b1ddf75882c3c5442a8981197a65b37e03ab6
Hugo Freire <hfreire@exec.sh>
2017-06-16 09:50:23 +0200

    test: add app test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
+#!/usr/bin/env node
+
 /*
  * Copyright (c) 2017, Hugo Freire <hugo@exec.sh>.
  *
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-#!/usr/bin/env node
 
 const VERSION = process.env.VERSION
 const VERSION_COMMIT = process.env.VERSION_COMMIT
