801528e326a6a0b731863b168463a7262fe4dd39
Hugo Freire <hfreire@exec.sh>
2017-11-29 20:12:45 +0100

    chore(copyright): update license filename

--- src/web/app/settings/index.ts
+++ src/web/app/settings/index.ts
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2017, Hugo Freire <hugo@exec.sh>.
  *
- * This source code is licensed under the license found in the 
+ * This source code is licensed under the license found in the
  * LICENSE.md file in the root directory of this source tree.
  */
 
