8552b86ea44b2504f829547a7ee0a2795b0bf4aa
Hugo Freire <hfreire@exec.sh>
2017-11-29 19:31:08 +0100

    chore(copyright): update license filename

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@
  * Copyright (c) 2017, Hugo Freire <hugo@exec.sh>.
  *
  * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
+ * LICENSE.md file in the root directory of this source tree.
  */
 
 const VERSION = process.env.VERSION
