8ff7c09710325b892cc37c5f8c7b2d8f30503787
Hugo Freire <hfreire@exec.sh>
2020-04-07 10:08:59 +0200

    chore: minor fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
+#!/usr/bin/env node
+
 /*
  * Copyright (c) 2020, Hugo Freire <hugo@exec.sh>.
  *
  * This source code is licensed under the license found in the
  * LICENSE.md file in the root directory of this source tree.
  */
-
-#!/usr/bin/env node
 
 const Logger = require('modern-logger')
 
